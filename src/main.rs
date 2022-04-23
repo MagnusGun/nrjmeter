45794a4615537ea9fec594ac085b52c10e50821c
Magnus Gunnarsson <magnus@gunnarssons.eu>
2022-04-23 20:53:49 +0200

    ok test implementation for reading input using rasberry pi gpio and a photodiode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use gpio_cdev::{Chip, EventRequestFlags, LineRequestFlags, EventType, LineEvent};
+
+
+fn do_main(ch :&str, port :u32) -> std::result::Result<(), gpio_cdev::Error> {
+    let mut chip = Chip::new(ch)?;
+    let line = chip.get_line(port)?;
+    let mut old:Option<LineEvent> = None;
+
+    for event in line.events(
+        LineRequestFlags::INPUT,
+        EventRequestFlags::BOTH_EDGES,
+        "gpioevents",
+    )? {
+        println!("{:?}", &event);
+        let evt = event?;
+        match evt.event_type() {
+            EventType::RisingEdge => {
+                if (old )
+                println!("{:?}", evt);
+            }
+            EventType::FallingEdge => {
+                println!("{:?}", evt);
+            }
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let res = do_main("/dev/gpiochip0", 16);
+
+    println!("{:?}",res);
 }
