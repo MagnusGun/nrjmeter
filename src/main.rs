fa2c047e3c4feacb694b84469a4fbbf22f785e6c
Magnus Gunnarsson <magnus@gunnarssons.eu>
2022-05-29 08:49:08 +0200

    added Nats and output to server instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use gpio_cdev::{Chip, EventRequestFlags, LineRequestFlags, EventType, LineEvent};
+use nats::Connection;
 use std::thread;
 use std::time::Duration;
+use sprintf::sprintf;
 
-fn do_main(ch :&str, port :u32) -> std::result::Result<(), gpio_cdev::Error> {
+
+
+fn do_main(ch :&str, port :u32, nc :&Connection) -> std::result::Result<(), gpio_cdev::Error> {
     let mut chip = Chip::new(ch)?;
     let input = chip.get_line(port)?;
     let output = chip.get_line(18)?;
@@ -32,7 +36,9 @@ fn do_main(ch :&str, port :u32) -> std::result::Result<(), gpio_cdev::Error> {
             EventType::RisingEdge => {
                 if !old.is_none() {
                     let period = evt.timestamp() - old.as_ref().unwrap().timestamp();
-                    println!("period(s):: {:?}\nnew_ts::{:?}\nold_ts::{:?}", period as f64/1000000000 as f64,  evt.timestamp(), old.unwrap().timestamp());
+                    //println!("period(s):: {:?}\nnew_ts::{:?}\nold_ts::{:?}", period as f64/1000000000 as f64,  evt.timestamp(), old.unwrap().timestamp());
+                    let result  = sprintf!("period(s):: {:?}\nnew_ts::{:?}\nold_ts::{:?}", period as f64/1000000000 as f64,  evt.timestamp(), old.unwrap().timestamp()).unwrap();
+                    nc.publish("nrjmeter", result)?;
                 }
                 old = Some(evt);
             }
@@ -45,8 +51,9 @@ fn do_main(ch :&str, port :u32) -> std::result::Result<(), gpio_cdev::Error> {
     Ok(())
 }
 
-fn main() {
-    let res = do_main("/dev/gpiochip0", 16);
-
-    println!("{:?}",res);
+fn main() -> std::io::Result<()> {
+    let nc = nats::connect("192.168.1.130")?;
+    let res = do_main("/dev/gpiochip0", 16, &nc);
+    //println!("{:?}",res);
+    Ok(())
 }
